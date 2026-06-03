4787a6311bbfec5c0b19b277e1db390ed9bc0b31
NicingMan-zzz <nakbelimcd@gmail.com>
2026-06-03 21:05:45 +0800

    Update HomeManagerEngine.java

--- OOP Assignment/HomeManagerEngine.java
+++ OOP Assignment/HomeManagerEngine.java
@@ -1,26 +1,36 @@
 import java.util.ArrayList;
+import javax.swing.DefaultListModel;
 
 public class HomeManagerEngine {
-    private ArrayList<SmartDevice> systemRegistry;
+    private DefaultListModel<SmartDevice> systemRegistryModel;
 
     public HomeManagerEngine() {
-        systemRegistry = new ArrayList<>();
+        systemRegistryModel = new DefaultListModel<>();
         seedDefaultInventory();
     }
 
     private void seedDefaultInventory() {
-        systemRegistry.add(new SmartClimateSystem("AC-1", "Living Room AC"));
-        systemRegistry.add(new SmartClimateSystem("AC-2", "Bedroom AC"));
-        systemRegistry.add(new SmartSecurityLight("LIGHT-1", "Living Room Light"));
-        systemRegistry.add(new SmartSecurityLight("LIGHT-2", "Bedroom Light"));
+        systemRegistryModel.addElement(new SmartClimateSystem("AC-1", "Living Room AC"));
+        systemRegistryModel.addElement(new SmartClimateSystem("AC-2", "Bedroom AC"));
+        systemRegistryModel.addElement(new SmartSecurityLight("LIGHT-1", "Living Room Light"));
+        systemRegistryModel.addElement(new SmartSecurityLight("LIGHT-2", "Bedroom Light"));
     }
 
-    public ArrayList<SmartDevice> getDevices() {
-        return systemRegistry;
+    public DefaultListModel<SmartDevice> getDeviceModel() {
+        return systemRegistryModel;
+    }
+
+    public ArrayList<SmartDevice> getDevicesAsList() {
+        ArrayList<SmartDevice> list = new ArrayList<>();
+        for (int i = 0; i < systemRegistryModel.size(); i++) {
+            list.add(systemRegistryModel.get(i));
+        }
+        return list;
     }
 
     public SmartDevice lookupDevice(String id) {
-        for (SmartDevice device : systemRegistry) {
+        for (int i = 0; i < systemRegistryModel.size(); i++) {
+            SmartDevice device = systemRegistryModel.get(i);
             if (device.getDeviceID().equalsIgnoreCase(id)) {
                 return device;
             }
@@ -31,7 +41,9 @@ public class HomeManagerEngine {
     public String generateSystemSummaryText() {
         StringBuilder reportBuffer = new StringBuilder();
         reportBuffer.append("--- LETS MAKE OUR HOME SMART TOGETHER ---\n\n");
-        for (SmartDevice device : systemRegistry) {
+        
+        for (int i = 0; i < systemRegistryModel.size(); i++) {
+            SmartDevice device = systemRegistryModel.get(i);
             reportBuffer.append(device.getDeviceDetails()).append("\n");
         }
         return reportBuffer.toString();
